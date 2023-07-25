f2f41e9f6d8f7f9d3b3a60239f9b948ac225eaaa
William Chang <wilicw1346@gmail.com>
2023-07-25 09:53:18 +0800

    fix(mq): use extern C to support CXX

--- Drivers/sl_i/Inc/mq.h
+++ Drivers/sl_i/Inc/mq.h
@@ -1,5 +1,4 @@
-#ifndef __MQ__
-#define __MQ__
+#pragma once
 
 #include <sli.h>
 #include <stdint.h>
@@ -10,10 +9,17 @@ typedef struct {
   uint32_t* msg;
 } mq_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 sli_err mq_create(mq_t* mq, uint32_t* space, uint32_t size);
 sli_err mq_send(mq_t* mq, uint32_t msg);
 sli_err mq_receive(mq_t* mq, uint32_t* buffer);
 sli_err mq_peek(mq_t* mq, uint32_t* buffer);
 sli_err mq_clear(mq_t* mq);
 
+#ifdef __cplusplus
+}
 #endif
+
